1fdc2239742fe0aeaf825ee2c43266ed4cc772fc
parikhaleghi <parisakhaleghi97@gmail.com>
2021-12-19 01:16:46 +0330

    style

--- ExceptionHandling/HandlerOptions.cs
+++ ExceptionHandling/HandlerOptions.cs
@@ -1,33 +1,32 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 
-namespace ExceptionHandling
+namespace ExceptionHandling;
+
+public class HandlerOptions
 {
-    public class HandlerOptions
+    public long LogMaxBodyLength { get; set; } = 4 * 1024;
+    public string OverSizeBodyLengthMessage { get; set; } = "the request body is too large to record";
+    public IResult OperationResult { get; set; } =
+        new Result<object>(null, OException.DefaultMessage);
+}
+
+public static class ExceptionHandlerMiddlewareExtension
+{
+    public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder app) =>
+        app.UseErrorHandler(new HandlerOptions());
+
+    public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder app,
+        HandlerOptions options)
     {
-        public long LogMaxBodyLength { get; set; } = 4 * 1024;
-        public string OverSizeBodyLengthMessage { get; set; } = "the request body is too large to record";
-        public IResult OperationResult { get; set; } =
-            new Result<object>(null, OException.DefaultMessage);
+        app.UseMiddleware<ExceptionHandlerMiddleware>(options);
+        return app;
     }
 
-    public static class ExceptionHandlerMiddlewareExtension
+    public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder app,
+        RequestDelegate exceptionHandler)
     {
-        public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder app) =>
-            app.UseErrorHandler(new HandlerOptions());
-
-        public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder app,
-            HandlerOptions options)
-        {
-            app.UseMiddleware<ExceptionHandlerMiddleware>(options);
-            return app;
-        }
-
-        public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder app,
-            RequestDelegate exceptionHandler)
-        {
-            app.UseMiddleware<ExceptionHandlerMiddleware>(exceptionHandler);
-            return app;
-        }
+        app.UseMiddleware<ExceptionHandlerMiddleware>(exceptionHandler);
+        return app;
     }
 }
